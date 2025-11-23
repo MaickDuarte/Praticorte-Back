212ea6cfb981a8c780636e466ee2e41a8c5d020b
Maick Duarte <maickduarte.2@hotmail.com>
2025-11-23 11:38:36 -0300

    getAppointmentByProviderAndDate

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -2,19 +2,34 @@ import { getAllDocs } from "../services/collectionBaseWorker.js";
 import { where } from "firebase/firestore";
 import { startOfDay, endOfDay } from 'date-fns';
 
+export const getAppointmentByProviderAndDate = async (req, res) => {
+    const { startDate, endDate, providerId } = req.query
+    const appointments = await getAllDocs({
+        collection: "agendamentos",
+        queries: [
+            where("provider.id", "==", providerId),
+            where("dateInfo.date", ">=", startOfDay(new Date(startDate))),
+            where("dateInfo.date", "<=", endOfDay(new Date(endDate)))
+        ],
+        res
+    })
+
+    if (!appointments) return
+
+    return res.json(appointments)
+}
+
 export const getAppointmentsByDate = async (req, res) => {
     const { startDate, endDate, establishmentId } = req.query
-        const start = new Date(startDate);
-        const end = new Date(endDate);
-        const appointments = await getAllDocs({
-            collection: "agendamentos",
-            queries: [
-                where("estabelecimentoId", "==", establishmentId),
-                where("dateInfo.date", ">=", startOfDay(start)),
-                where("dateInfo.date", "<=", endOfDay(end))
-            ],
-            res
-        })
+    const appointments = await getAllDocs({
+        collection: "agendamentos",
+        queries: [
+            where("estabelecimentoId", "==", establishmentId),
+            where("dateInfo.date", ">=", startOfDay(new Date(startDate))),
+            where("dateInfo.date", "<=", endOfDay(new Date(endDate)))
+        ],
+        res
+    })
 
     if (!appointments) return
 
--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAppointmentsByDate } from "../controllers/appointment.controller.js";
+import { getAppointmentsByDate, getAppointmentByProviderAndDate } from "../controllers/appointment.controller.js";
 
 const router = express.Router();
 
 router.get("/getAppointmentsByDate", getAppointmentsByDate);
+router.get("/getAppointmentByProviderAndDate", getAppointmentByProviderAndDate);
 
 export default router;
